23ed8e7acd5431fa0874f06ce2561f2edd77eb80
emyrue <94935633+emyrue@users.noreply.github.com>
2022-02-28 13:02:23 -0700

    Fixed event listeners

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
-
-
 let mobileItem = document.querySelectorAll('.mobile-menu-item');
-let close = document.querySelector('.close');
+let closeButton = document.querySelector('.close');
 let hamburger = document.querySelector('.menu');
 let menuOpen = document.querySelector('.menu-open');
 
@@ -9,7 +7,8 @@ function display() {
     menuOpen.classList.toggle('show');
 }
 
-mobileItem.addEventListener('click', display);
-close.addEventListener('click', display);
+closeButton.addEventListener('click', display);
 hamburger.addEventListener('click', display);
-
+for (let i = 0; i < mobileItem.length; i++) {
+    mobileItem[i].addEventListener('click', display);
+}
