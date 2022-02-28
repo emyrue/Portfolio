68704f4226aa0d934dc77f01afe7914635677e06
emyrue <94935633+emyrue@users.noreply.github.com>
2022-02-28 13:07:00 -0700

    Fixed linter errors

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
-let mobileItem = document.querySelectorAll('.mobile-menu-item');
-let closeButton = document.querySelector('.close');
-let hamburger = document.querySelector('.menu');
-let menuOpen = document.querySelector('.menu-open');
+const mobileItem = document.querySelectorAll('.mobile-menu-item');
+const closeButton = document.querySelector('.close');
+const hamburger = document.querySelector('.menu');
+const menuOpen = document.querySelector('.menu-open');
 
 function display() {
-    menuOpen.classList.toggle('show');
+  menuOpen.classList.toggle('show');
 }
 
 closeButton.addEventListener('click', display);
 hamburger.addEventListener('click', display);
 for (let i = 0; i < mobileItem.length; i++) {
-    mobileItem[i].addEventListener('click', display);
+  mobileItem[i].addEventListener('click', display);
 }
