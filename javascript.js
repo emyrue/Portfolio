3ac8d6035f10db198c03f015309fc0ff8eb59bc4
emyrue <94935633+emyrue@users.noreply.github.com>
2022-03-02 12:20:03 -0700

    Minimizing length of code

--- javascript.js
+++ javascript.js
@@ -13,7 +13,7 @@ for (let i = 0; i < mobileItem.length; i += 1) {
   mobileItem[i].addEventListener('click', display);
 }
 
-const project = {
+const project = [{
   name: 'Project name goes here',
   description: 'Lorem ipsum dolor sit amet,'
   + 'consectetur adipiscing elit, sed do eiusmod '
@@ -41,16 +41,16 @@ const project = {
   previous: './images/previous.png',
   liveVersionLink: 'https://emyrue.github.io/Portfolio',
   sourceLink: 'https://github.com/emyrue/Portfolio/',
-};
+}];
 
 const section = document.createElement('section');
 const xIcon = document.createElement('img');
-const heading = document.createElement('h2');
-const list = document.createElement('ul');
-const listItem1 = document.createElement('li');
-const listItem2 = document.createElement('li');
-const listItem3 = document.createElement('li');
-const slideshow = document.createElement('div');
+//const heading = document.createElement('h2');
+//const list = document.createElement('ul');
+//const listItem1 = document.createElement('li');
+//const listItem2 = document.createElement('li');
+//const listItem3 = document.createElement('li');
+//const slideshow = document.createElement('div');
 const arrowLeft = document.createElement('img');
 const arrowRight = document.createElement('img');
 const slides = document.createElement('div');
@@ -76,13 +76,13 @@ const previous = document.createElement('img');
 const next = document.createElement('img');
 
 section.className = 'default';
-xIcon.className = 'close-icon';
-heading.className = 'popup-heading';
-list.className = 'popup-list';
-listItem1.classList.add('popup-list-item', 'popup-list-item1');
-listItem2.classList.add('popup-list-item', 'popup-list-item2');
-listItem3.classList.add('popup-list-item', 'popup-list-item3');
-slideshow.className = 'slideshow';
+//xIcon.className = 'close-icon';
+//heading.className = 'popup-heading';
+//list.className = 'popup-list';
+//listItem1.classList.add('popup-list-item', 'popup-list-item1');
+//listItem2.classList.add('popup-list-item', 'popup-list-item2');
+//listItem3.classList.add('popup-list-item', 'popup-list-item3');
+//slideshow.className = 'slideshow';
 arrowLeft.className = 'arrow';
 arrowRight.className = 'arrow';
 slides.className = 'slides';
@@ -104,58 +104,74 @@ nextPrevious.className = 'next-previous';
 previous.className = 'previous';
 next.className = 'next';
 
-xIcon.src = project.closePopup;
-heading.textContent = project.name;
-listItem1.textContent = project.item1;
-listItem2.textContent = project.item2;
-listItem3.textContent = project.item3;
-arrowLeft.src = project.leftArrow;
-arrowRight.src = project.rightArrow;
-image1.src = project.imageURL;
-image2.src = project.imageURL;
-image3.src = project.imageURL;
-image4.src = project.imageURL;
-paragraph1.textContent = project.description;
-paragraph2.textContent = project.description2;
-link1.href = project.liveVersionLink;
-span1.textContent = project.live;
-liveImage.src = project.liveImage;
-link2.href = project.sourceLink;
-span2.textContent = project.source;
-sourceImage.src = project.sourceImage;
-previous.src = project.previous;
-next.src = project.next;
+xIcon.src = project[0].closePopup;
+//heading.textContent = project[0].name;
+//listItem1.textContent = project[0].item1;
+//listItem2.textContent = project[0].item2;
+//listItem3.textContent = project[0].item3;
+//arrowLeft.src = project[0].leftArrow;
+//arrowRight.src = project[0].rightArrow;
+image1.src = project[0].imageURL;
+image2.src = project[0].imageURL;
+image3.src = project[0].imageURL;
+image4.src = project[0].imageURL;
+paragraph1.textContent = project[0].description;
+paragraph2.textContent = project[0].description2;
+link1.href = project[0].liveVersionLink;
+span1.textContent = project[0].live;
+liveImage.src = project[0].liveImage;
+link2.href = project[0].sourceLink;
+span2.textContent = project[0].source;
+sourceImage.src = project[0].sourceImage;
+previous.src = project[0].previous;
+next.src = project[0].next;
 
 document.body.appendChild(section);
-section.appendChild(xIcon);
-section.appendChild(heading);
-section.appendChild(list);
-section.appendChild(slideshow);
-list.appendChild(listItem1);
-list.appendChild(listItem2);
-list.appendChild(listItem3);
-slideshow.appendChild(arrowLeft);
-slideshow.appendChild(arrowRight);
-section.appendChild(slides);
-slides.appendChild(image1);
-slides.appendChild(image2);
-slides.appendChild(image3);
-slides.appendChild(image4);
-section.appendChild(flexContainer);
-flexContainer.appendChild(paragraph1);
-flexContainer.appendChild(paragraph2);
-section.appendChild(buttons);
-buttons.appendChild(button1);
-button1.appendChild(link1);
-link1.appendChild(span1);
-link1.appendChild(liveImage);
-buttons.appendChild(button2);
-button2.appendChild(link2);
-link2.appendChild(span2);
-link2.appendChild(sourceImage);
-section.appendChild(nextPrevious);
-nextPrevious.appendChild(previous);
-nextPrevious.appendChild(next);
+//section.appendChild(xIcon);
+//section.appendChild(heading);
+//section.appendChild(list);
+//section.appendChild(slideshow);
+//list.appendChild(listItem1);
+//list.appendChild(listItem2);
+//list.appendChild(listItem3);
+//slideshow.appendChild(arrowLeft);
+//slideshow.appendChild(arrowRight);
+//section.appendChild(slides);
+//slides.appendChild(image1);
+//slides.appendChild(image2);
+//slides.appendChild(image3);
+//slides.appendChild(image4);
+//section.appendChild(flexContainer);
+//flexContainer.appendChild(paragraph1);
+//flexContainer.appendChild(paragraph2);
+//section.appendChild(buttons);
+//buttons.appendChild(button1);
+//button1.appendChild(link1);
+//link1.appendChild(span1);
+//link1.appendChild(liveImage);
+//buttons.appendChild(button2);
+//button2.appendChild(link2);//
+//link2.appendChild(span2);
+//link2.appendChild(sourceImage);
+//section.appendChild(nextPrevious);
+//nextPrevious.appendChild(previous);
+//nextPrevious.appendChild(next);
+
+const markup = `
+  <img class="close-icon" src=${project[0].closePopup}>
+  <h2 class="popup-heading">${project[0].name}</h2>
+  <ul class="popup-list">
+    <li class="popup-list-item">${project[0].item1}</li>
+    <li class="popup-list-item popup-list-item2">${project[0].item2}</li>
+    <li class="popup-list-item">${project[0].item3}</li>
+  </ul>
+  <div class="slideshow">
+    <img class="arrow" src=${project[0].leftArrow}>
+    <img class="arrow" src=${project[0].rightArrow}>
+  </div>
+`;
+
+section.innerHTML = markup;
 
 function display2() {
   section.classList.toggle('popup');
